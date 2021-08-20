b3496577ad55c7444801ada444ed517a77b8fbf5
Thirupathi Peraboina <peraboinathirupathi@outlook.com>
2021-08-20 13:51:23 +0530

    Update ownfile.ps1

--- ownfile.ps1
+++ ownfile.ps1
@@ -7,13 +7,23 @@ Set-Alias -Name dc docker-compose
 Set-Alias -Name hl helm
 
 # Kubernetes short cuts
-function klo([Parameter(ValueFromRemainingArguments = $true)]$params) { & kubectl logs -f $params }
-function kex([Parameter(ValueFromRemainingArguments = $true)]$params) { & kubectl exec -i -t $params }
+function kcg([Parameter(ValueFromRemainingArguments = $true)]$params) { & kubectl config get-contexts $params }
+function kc([Parameter(ValueFromRemainingArguments = $true)]$params) { & kubectl config use-context $params }
+
+#kubectl get
 function kg([Parameter(ValueFromRemainingArguments = $true)]$params) { & kubectl get $params }
-function kd([Parameter(ValueFromRemainingArguments = $true)]$params) { & kubectl describe $params }
 function kdpo([Parameter(ValueFromRemainingArguments = $true)]$params) { & kubectl describe pods $params }
 function kgdep([Parameter(ValueFromRemainingArguments = $true)]$params) { & kubectl get deployment $params }
-function kddep([Parameter(ValueFromRemainingArguments = $true)]$params) { & kubectl describe deployment $params }
 function kgoyaml([Parameter(ValueFromRemainingArguments = $true)]$params) { & kubectl get -o=yaml $params }
+
+#kubectl describe
+function kd([Parameter(ValueFromRemainingArguments = $true)]$params) { & kubectl describe $params }
+function kddep([Parameter(ValueFromRemainingArguments = $true)]$params) { & kubectl describe deployment $params }
+
+#kubectl edit
 function kepo([Parameter(ValueFromRemainingArguments = $true)]$params) { & kubectl edit pod $params }
 function kedep([Parameter(ValueFromRemainingArguments = $true)]$params) { & kubectl edit deployment $params }
+
+#kubectl log
+function klo([Parameter(ValueFromRemainingArguments = $true)]$params) { & kubectl logs -f $params }
+function kex([Parameter(ValueFromRemainingArguments = $true)]$params) { & kubectl exec -i -t $params }
